e8466332c27196f134d5fa3b35234ace2371e5a6
icwells <smrupp16@gmail.com>
2018-06-29 12:52:04 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -15,7 +15,11 @@ rm -r build/
 rm *.c
 cd ../
 
-mv src/$KT.*.so bin/$KT.so
-mv src/$SP.*.so bin/$SP.so
-mv src/$SS.*.so bin/$SS.so
-mv src/$TS.*.so bin/$TS.so
+mv src/$KT.so bin/$KT.so
+mv src/$SP.so bin/$SP.so
+mv src/$SS.so bin/$SS.so
+mv src/$TS.so bin/$TS.so
+
+echo ""
+echo "Done"
+echo ""
