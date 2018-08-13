37d50dde8b713b6cfd268155558db224db4f06f8
icwells <smrupp16@gmail.com>
2018-08-13 15:24:15 -0700

    Added ability to sort out scientific name mathces to checkTaxonomies

--- bin/checkTaxonomies.py
+++ bin/checkTaxonomies.py
@@ -1,8 +1,8 @@
 '''This script will assign taxonomies from a manually curated dataset to entries with matching common/scientific names.'''
 
-from argparse import ArgumentParser
 import os
 from datetime import datetime
+from argparse import ArgumentParser
 
 def writeMissed(fails, misses):
 	# Write missed to file
@@ -10,6 +10,29 @@ def writeMissed(fails, misses):
 	with open(fails, "w") as out:
 		for i in misses:
 			out.write(i)
+
+def checkNames(infile, outfile):
+	# Checks search term against matched species name
+	first = True
+	misses = []
+	count = 0
+	print("\n\tComparing search terms against returned species names...")
+	with open(outfile, "w") as out:
+		with open(infile, "r") as f:
+			for line in f:
+				if first == False:
+					s = line.split(",")
+					if s[1].lower().strip() == s[8].lower().strip():
+						out.write(line)
+						count += 1
+					else:
+						misses.append(line)
+				else:
+					# Write header and save for missed file
+					out.write(line)
+					misses.append(line)
+					first = False
+	return misses, count
 
 def matchTaxa(infile, outfile, taxa, head):
 	# Writes matched taxonomies to passes and returns misses
@@ -64,17 +87,14 @@ def getTaxa(infile):
 
 def checkArgs(args):
 	# Makes sure args are present
-	if not args.i:
-		print("\n\t[Error] Please specify an input file. Exiting.\n")
+	if not args.i and args.o:
+		print("\n\t[Error] Please specify an input file and an output directory. Exiting.\n")
 		quit()
-	if not args.r:
-		print("\n\t[Error] Please specify a reference taxonomy file. Exiting.\n")
-		quit()
-	if not args.r:
-		print("\n\t[Error] Please specify an output directory. Exiting.\n")
+	if not args.r and not args.checkNames:
+		print("\n\t[Error] Please specify either -r <reference_taxonomy> or --checkNames. Exiting.\n")
 		quit()
 	for i in [args.i, args.r]:
-		if not os.path.isfile(i):
+		if i and not os.path.isfile(i):
 			print(("\n\t[Error] Cannot find {}. Exiting.\n").format(i))
 	# Format outdir
 	if args.o[-1] != "/":
@@ -84,21 +104,29 @@ def checkArgs(args):
 	# Get outfile names
 	name = os.path.split(args.i)[1]
 	name = name[:name.find(".")]
-	return args.o + name + ".withTaxa.csv", args.o + name + ".unmatched.csv",
+	if args.checkNames:
+		return args.o + name + ".Passed.csv", args.o + name + ".failed.csv",
+	else:
+		return args.o + name + ".withTaxa.csv", args.o + name + ".unmatched.csv",
 
 def main():
 	starttime = datetime.now()
-	parser = ArgumentParser("This script will assign taxonomies from a manually \
-curated dataset to entries with matching common/scientific names.")
-	parser.add_argument("-i", help = "Path to input file (output of kestrel.py --extract).")
-	parser.add_argument("-r", 
-help = "Path to reference file of manually currated taxonomies (output of previous kestrel search).")
+	parser = ArgumentParser("This script will assist with manually curating Kestrel taxonomy results.")
+	parser.add_argument("--checkNames", action = "store_true", default = False,
+help = "Compares species name to search term (for identifying corectly matched scientific search terms).")
+	parser.add_argument("-i", help = "Path to input file.")
+	parser.add_argument("-r", help = "Path to reference file of manually currated taxonomies \
+(output of previous kestrel search; will write curated taxonomies to one file and unmatched taxonomies to another).")
 	parser.add_argument("-o", help = "Path to output directory.")
 	args = parser.parse_args()
 	passes, fails = checkArgs(args)
-	head, taxa = getTaxa(args.r)
-	misses, l = matchTaxa(args.i, passes, taxa, head)
-	writeMissed(fails, misses)
+	if args.checkNames:
+		misses, l = checkNames(args.i, passes)
+		writeMissed(fails, misses)
+	else:
+		head, taxa = getTaxa(args.r)
+		misses, l = matchTaxa(args.i, passes, taxa, head)
+		writeMissed(fails, misses)
 	print(("\n\tFound matches for {} of {} queries.").format(l, len(misses)+l))
 	print(("\tFinished. Runtime: {}\n").format(datetime.now()-starttime))
 
